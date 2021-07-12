c2c8169581265f0fb0696bc4cf5cfe9336a3cb1d
Camilla Correia <camillajesuscorreia@hotmail.com>
2021-07-12 18:19:06 -0300

    feat: authentication module structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'conta',
+  loadChildren: () => import('./conta/conta.module')
+    .then(m => m.ContaModule)
+  },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
